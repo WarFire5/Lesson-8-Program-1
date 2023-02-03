70114d12dda99893cf494fd8f357319c6e2721e1
Татьяна <@WIN-E362SIULNK8>
2023-02-04 00:20:19 +0300

    Добавьте файлы проекта.

---
+++ Lesson 8 Program 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_8_Program_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Чётное");
+            }
+
+            else
+            {
+                Console.WriteLine("Нечётное");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
